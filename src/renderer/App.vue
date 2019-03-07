a693143b551b3fc0e9611649134cf4d2133464bb
Tyrival <tyrival@qq.com>
2019-03-07 09:29:47 +0800

    初始化工程

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,59 +1,19 @@
 <template>
 	<div id="app">
-		<div v-if="getOsInfo().name === 'Mac'" class="title">HDFSKit</div>
+		<div v-if="showTitle" class="title">HDFSKit</div>
 		<router-view></router-view>
 	</div>
 </template>
 
 <script>
   import './assets/styles/app.less'
+  import OSUtils from './utils/OSUtils'
 
   export default {
     name: 'HDFSKit',
-    methods: {
-      getOsInfo () {
-        let userAgent = navigator.userAgent.toLowerCase()
-        let name = 'Unknown'
-        let version = 'Unknown'
-        if (userAgent.indexOf('win') > -1) {
-          name = 'Windows'
-          if (userAgent.indexOf('windows nt 5.0') > -1) {
-            version = 'Windows 2000'
-          } else if (userAgent.indexOf('windows nt 5.1') > -1 || userAgent.indexOf('windows nt 5.2') > -1) {
-            version = 'Windows XP'
-          } else if (userAgent.indexOf('windows nt 6.0') > -1) {
-            version = 'Windows Vista'
-          } else if (userAgent.indexOf('windows nt 6.1') > -1 || userAgent.indexOf('windows 7') > -1) {
-            version = 'Windows 7'
-          } else if (userAgent.indexOf('windows nt 6.2') > -1 || userAgent.indexOf('windows 8') > -1) {
-            version = 'Windows 8'
-          } else if (userAgent.indexOf('windows nt 6.3') > -1) {
-            version = 'Windows 8.1'
-          } else if (userAgent.indexOf('windows nt 6.2') > -1 || userAgent.indexOf('windows nt 10.0') > -1) {
-            version = 'Windows 10'
-          } else {
-            version = 'Unknown'
-          }
-        } else if (userAgent.indexOf('iphone') > -1) {
-          name = 'Iphone'
-        } else if (userAgent.indexOf('mac') > -1) {
-          name = 'Mac'
-        } else if (userAgent.indexOf('x11') > -1 || userAgent.indexOf('unix') > -1 || userAgent.indexOf('sunname') > -1 || userAgent.indexOf('bsd') > -1) {
-          name = 'Unix'
-        } else if (userAgent.indexOf('linux') > -1) {
-          if (userAgent.indexOf('android') > -1) {
-            name = 'Android'
-          } else {
-            name = 'Linux'
-          }
-        } else {
-          name = 'Unknown'
-        }
-        let os = {
-          name: name,
-          version: version
-        }
-        return os
+    computed: {
+      showTitle () {
+        return OSUtils.getOsInfo().name === 'Mac'
       }
     }
   }
